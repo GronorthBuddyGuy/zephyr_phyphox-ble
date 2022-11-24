f3d1d0d3f7ae2a9a0bef4e4c2af35f008c83fd29
Victor Francisco Chavez Bermudez <chavez-bermudez@fh-aachen.de>
2022-11-24 15:52:01 +0100

    Readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Zephyr Phyphox BLE'
+copyright = '2022, Victor Chavez'
+author = 'Victor Chavez'
+
+version = '1.0.0'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [ 'breathe',
+                'sphinx_rtd_theme']
+
+# Breathe Configuration
+breathe_projects = {"phyphox": "xml/"}
+breathe_default_project = "phyphox"
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. ZephyrPhyphoxBLe documentation master file, created by
+   sphinx-quickstart on Thu Nov 24 14:18:05 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Zephyr Phyphox BLE module API documentation
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+.. doxygennamespace:: phyphox_ble
+   :project: phyphox
+   
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
